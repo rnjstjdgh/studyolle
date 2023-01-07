15cea6e66d5a166353f817ea9832c96516caabf5
rnjstjdgh <gshgsh0831@daum.net>
2023-01-07 15:38:35 +0900

    [번외] jpa repository 메소드 리턴 값들은 항상 nullable 하게 만들어야하나?

---
+++ src/test/kotlin/com/soungho/studyolle/account/AccountRepositoryTest.kt
@@ -0,0 +1,21 @@
+package com.soungho.studyolle.account
+
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+
+@DataJpaTest
+class AccountRepositoryTest @Autowired constructor(
+    private val accountRepository: AccountRepository
+) {
+
+    @DisplayName("null 허용 안하는 타입에대해 없을때 어떻게 되는지?")
+    @Test
+    fun not_allow_null_test() {
+        val account = accountRepository.findByEmail("soungho")
+        assertNull(account)
+    }
+}
